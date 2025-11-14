4c802531e538ca827b46f4d042c66d9324b06389
muskan9567 <154818290+muskan9567@users.noreply.github.com>
2025-11-14 23:58:24 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>  //fragments
+
+    <div id = 'muskan'>
+      <h1>hello </h1>
+    </div>
+    <div id = 'singh'>
+      <h1>hey hiiii </h1>
+    </div>
+
+    </>
+    
+  )
+}
+
+export default App
